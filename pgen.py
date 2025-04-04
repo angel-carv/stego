ccc9b06f49ef8725538281e122f9a6de8e1c2827
angel-carv <angelcarvajal323@gmail.com>
2025-04-04 11:44:15 -0700

    Work on button function. Needs to display correctly on a loop.

--- pgen.py
+++ pgen.py
@@ -2,13 +2,14 @@ import secrets
 import string
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox
 
 global pLength 
 pLength = 15
 
-#root = tk.Tk()
-#root.geometry("600x600")
-#root.title("HELLO")
+root = tk.Tk()
+root.geometry("600x600")
+root.title("HELLO")
 
 lowercase = string.ascii_lowercase
 uppercase = string.ascii_uppercase
@@ -16,12 +17,7 @@ specials = "!@#$%^&*"
 numbers = list(str(i) for i in range(10))
 
 validChars = list(lowercase + uppercase + specials) + numbers
-#print(validChars)
     
-
-
-
-
 def genPassword():
     i = 0
     password = ""
@@ -29,27 +25,21 @@ def genPassword():
         password += secrets.choice(validChars)
         i+=1
     return password
-
-
-print(genPassword())
+def showPassword():
+    messagebox.showinfo("Password Generated", print(genPassword()))
 
     
 
+welcome = tk.Label(root, text="Generate a password" )
+welcome.pack(pady=10)
+testEntry = ttk.Entry(root , width= 50)
+testEntry.pack(pady=20)
+
+goButton = ttk.Button(root, text="Generate 0:", command=showPassword())
+goButton.pack(pady= 15)
+
+displayPass = tk.Label()
 
 
-
-
-
-
-
-
-#welcome = tk.Label(root, text="Generate a password" )
-#welcome.pack(pady=10)
-#testEntry = ttk.Entry(root , width= 50)
-#testEntry.pack(pady=20)
-
-#goButton = ttk.Button(root, text="Generate 0:")
-#goButton.pack(pady= 15)
-
-#root.mainloop()
+root.mainloop()
 
