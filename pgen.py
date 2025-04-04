8b2db0d367365d2c3edd914335f807618ef65be5
angel-carv <angelcarvajal323@gmail.com>
2025-04-04 11:36:55 -0700

    pgen function works

--- pgen.py
+++ pgen.py
@@ -3,9 +3,12 @@ import string
 import tkinter as tk
 from tkinter import ttk
 
-root = tk.Tk()
-root.geometry("600x600")
-root.title("HELLO")
+global pLength 
+pLength = 15
+
+#root = tk.Tk()
+#root.geometry("600x600")
+#root.title("HELLO")
 
 lowercase = string.ascii_lowercase
 uppercase = string.ascii_uppercase
@@ -13,17 +16,25 @@ specials = "!@#$%^&*"
 numbers = list(str(i) for i in range(10))
 
 validChars = list(lowercase + uppercase + specials) + numbers
-print(validChars)
+#print(validChars)
     
 
 
 
 
-#def generatePass():
+def genPassword():
+    i = 0
+    password = ""
+    while(i != pLength):
+        password += secrets.choice(validChars)
+        i+=1
+    return password
+
+
+print(genPassword())
 
     
 
-    
 
 
 
@@ -31,13 +42,14 @@ print(validChars)
 
 
 
-welcome = tk.Label(root, text="Generate a password" )
-welcome.pack(pady=10)
-testEntry = ttk.Entry(root , width= 50)
-testEntry.pack(pady=20)
 
-goButton = ttk.Button(root, text="Generate 0:")
-goButton.pack(pady= 15)
+#welcome = tk.Label(root, text="Generate a password" )
+#welcome.pack(pady=10)
+#testEntry = ttk.Entry(root , width= 50)
+#testEntry.pack(pady=20)
 
-root.mainloop()
+#goButton = ttk.Button(root, text="Generate 0:")
+#goButton.pack(pady= 15)
+
+#root.mainloop()
 
